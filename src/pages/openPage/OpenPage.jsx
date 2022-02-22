f9fcde638c17fe0ed2438429dd31fccdbf6ce156
Arthur <newartroll11@gmail.com>
2022-02-22 10:43:29 +0400

    style:(first page adaptive)

--- src/pages/openPage/OpenPage.jsx
+++ src/pages/openPage/OpenPage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Logregheader from "../../components/logRegHeader/LogRegHeader";
 import Logregmain from "../../components/logRegMain/LogRegMain";
+import styles from "./OpenPage.module.css";
 
 const Openpage = () => {
   return (
-    <>
+    <div className={styles.openPage}>
       <Logregheader />
       <Logregmain />
-    </>
+    </div>
   );
 };
 
