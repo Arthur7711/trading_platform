1f3df642ba3bd3719d29a2b95ce1f8536b0db534
Arthur <newartroll11@gmail.com>
2022-03-02 16:06:15 +0400

    feat:(scanner page creating)

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import Markets from "./pages/market/Markets";
 import Myportfolio from "./pages/myPortfolio/MyPortfolio";
 import Openpage from "./pages/openPage/OpenPage";
 import Register from "./pages/register/Register";
+import Scanner from "./pages/scanner/Scanner";
 import Toptraders from "./pages/topTraders/TopTraders";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Openpage />} />
+        <Route exact path="/" element={<Openpage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/portfolio" element={<Myportfolio />} />
         <Route path="/top" element={<Toptraders />} />
         <Route path="/market" element={<Markets />} />
+        <Route path="/scanner" element={<Scanner />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/scanner/Scanner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Main from "../../components/main/Main";
+import styles from "./Scanner.module.css";
+
+const Scanner = () => {
+  return <Main>Scanner</Main>;
+};
+
+export default Scanner;
