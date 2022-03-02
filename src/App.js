963b144b6676176094707042eb680c095d3709ef
Arthur <newartroll11@gmail.com>
2022-03-02 15:57:27 +0400

    feat:(portfolio page creating)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Login from "./pages/login/Login";
+import Myportfolio from "./pages/myPortfolio/MyPortfolio";
 import Openpage from "./pages/openPage/OpenPage";
 import Register from "./pages/register/Register";
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/portfolio" element={<Myportfolio />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/myPortfolio/MyPortfolio.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Main from "../../components/main/Main";
+import styles from "./Myportfolio.module.css";
+
+const Myportfolio = () => {
+  return <Main>Myportfolio</Main>;
+};
+
+export default Myportfolio;
