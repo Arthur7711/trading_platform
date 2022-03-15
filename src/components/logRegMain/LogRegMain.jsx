efd3dc35800e3e1207307a4cc4f022ee5e74a273
Arthur <newartroll11@gmail.com>
2022-03-15 11:00:22 +0400

    feat:(plan get items)

--- src/components/logRegMain/LogRegMain.jsx
+++ src/components/logRegMain/LogRegMain.jsx
@@ -1,115 +1,32 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Paymantitem from "../paymantItem/PaymantItem";
 import styles from "./Logregmain.module.css";
 import { API } from "../../API/API";
 
 const Logregmain = () => {
+  const [data, setData] = useState([]);
 
-  const fakeData = [
-    {
-      data: [
-        { supported: true, title: "Social aspect" },
-        { supported: true, title: "Can talk amd chat" },
-        { supported: true, title: "See free trade alerts users put out" },
-        {
-          supported: true,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-        {
-          supported: false,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-        {
-          supported: false,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-        {
-          supported: true,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-        {
-          supported: false,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-        {
-          supported: false,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-      ],
-      cost: "FREE",
-    },
-    {
-      data: [
-        { supported: true, title: "Social aspect" },
-        { supported: true, title: "Can talk amd chat" },
-        { supported: true, title: "See free trade alerts users put out" },
-        {
-          supported: true,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-        {
-          supported: true,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-        {
-          supported: false,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-        {
-          supported: true,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-        {
-          supported: true,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-        {
-          supported: false,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-      ],
-      cost: "$ 54",
-    },
-    {
-      data: [
-        { supported: true, title: "Social aspect" },
-        { supported: true, title: "Can talk amd chat" },
-        { supported: true, title: "See free trade alerts users put out" },
-        {
-          supported: true,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-        {
-          supported: true,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-        {
-          supported: true,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-        {
-          supported: true,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-        {
-          supported: true,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-        {
-          supported: true,
-          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        },
-      ],
-      cost: "$ 120",
-    },
-  ];
+  async function getPlans() {
+    const res = await API.get("/plans");
+    const newdata = await res.data.success;
+    setData([...data, ...newdata]);
+  }
+  useEffect(() => {
+    getPlans();
+  }, []);
+
   return (
     <main className={styles.main}>
-      {fakeData.map((el, i) => (
-        <Fragment key={i}>
-          <Paymantitem fakeData={el.data} cost={el.cost} />
-        </Fragment>
-      ))}
+      {data &&
+        data.map((el, i) => (
+          <Fragment key={i}>
+            <Paymantitem
+              name={el.name}
+              fakeData={el.description}
+              cost={el.price}
+            />
+          </Fragment>
+        ))}
     </main>
   );
 };
--- src/components/paymantItem/PaymantItem.jsx
+++ src/components/paymantItem/PaymantItem.jsx
@@ -2,22 +2,29 @@ import React from "react";
 import styles from "./PaymantItem.module.css";
 import { ReactComponent as DoneSvg } from "../../assets/images/done.svg";
 import { ReactComponent as ErrorSvg } from "../../assets/images/error.svg";
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-const Paymantitem = ({ fakeData, cost }) => {
-
+const Paymantitem = ({ name, fakeData, cost }) => {
   const navigate = useNavigate();
-
+  function selectingPlan(element) {
+    localStorage.setItem("paymant", element);
+    return navigate("/register");
+  }
   return (
-    <div className={styles.item}>
-      {fakeData.map((el, i) => (
-        <div key={i} className={styles.eachElement}>
-          {el.supported === false ? <ErrorSvg /> : <DoneSvg />}
-          <p>{el.title}</p>
-        </div>
-      ))}
-      <div className={styles.line}></div>
-      <p className={styles.cost} onClick={()=>navigate('/register')}>{cost}</p>
+    <div>
+      <p className={styles.title}>{name}</p>
+      <div className={styles.item}>
+        {fakeData.map((el, i) => (
+          <div key={i} className={styles.eachElement}>
+            {el.supported === false ? <ErrorSvg /> : <DoneSvg />}
+            <p>{el.title}</p>
+          </div>
+        ))}
+        <div className={styles.line}></div>
+        <p className={styles.cost} onClick={() => selectingPlan(cost)}>
+          {cost === 0 ? "FREE" : `$${cost}`}
+        </p>
+      </div>
     </div>
   );
 };
