e2eafa87358c54acbafed7442870aef0e6546c4d
Arthur <newartroll11@gmail.com>
2022-03-23 13:01:13 +0400

    style:(trades area)

--- src/components/tradesChart/TradesChart.jsx
+++ src/components/tradesChart/TradesChart.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { AdvancedRealTimeChart } from "react-ts-tradingview-widgets";
-import styles from './Tradeschart.module.css'
+import styles from "./Tradeschart.module.css";
 
 const Tradeschart = () => {
   return (
-    <div className={styles.advanced}>
-      <AdvancedRealTimeChart
-        theme="dark"
-        width={550}
-        height={400}
-      ></AdvancedRealTimeChart>
+    <div className={styles.mainPart}>
+      <p className={styles.title}>Market Scanner</p>
+      <div className={styles.advanced}>
+        <AdvancedRealTimeChart
+          theme="dark"
+          width={550}
+          height={400}
+        ></AdvancedRealTimeChart>
+      </div>
     </div>
   );
 };
