629e0c42db3b732b6fb01e3e2b9a6788ee0783f8
Arthur <newartroll11@gmail.com>
2022-03-29 17:22:40 +0400

    feat:(profile tible )

--- src/pages/myPortfolio/MyPortfolio.jsx
+++ src/pages/myPortfolio/MyPortfolio.jsx
@@ -1,14 +1,46 @@
 import React from "react";
 import Main from "../../components/main/Main";
-import MuiSelector from "../../components/muiSelector/MuiSelector";
 import styles from "./Myportfolio.module.css";
+import MyTabs from "../../components/tabs/tabs";
+import Profiletable from "../../components/profileTable/ProfileTable";
 
 const Myportfolio = () => {
-  return <Main>
-    <div>
-      <MuiSelector />
-    </div>
-  </Main>;
+  const tabsItems = [
+    "All",
+    "Indices Futures",
+    "Stocs",
+    "Traders",
+    "Subscriptions",
+    "Stocings",
+    "Subscribe",
+  ];
+  const tabsData = {
+    All: <Profiletable />,
+    "Indices Futures": "Indices Futures",
+    Stocs: "Stocs",
+    Traders: "Traders",
+    Subscriptions: "Subscriptions",
+    Stocings: "Stocings",
+    Subscribe: "Subscribe",
+  };
+  return (
+    <Main>
+      <div>
+        <div>
+          <select className={styles.mySelect}>
+            <option value="Mywatchlist">My watchlist</option>
+            <option value="Allwatchlist">All watchlist</option>
+            <option value="Recentlyinvested">Recently invested</option>
+          </select>
+        </div>
+        <main className={styles.topMain}>
+          <div>
+            <MyTabs WIDTHSIZE={"100%"} tabsData={tabsData} arrs={tabsItems} />
+          </div>
+        </main>
+      </div>
+    </Main>
+  );
 };
 
 export default Myportfolio;
